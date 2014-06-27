6774eaed6ff4b836b70dda11ee09cb18b3d83a70
vasu-naman <github@vyrazu.com>
2014-06-28 00:58:06 +0530

    Profile and update media added
    Vasu Naman

---
+++ updatemedia.php
@@ -0,0 +1,129 @@
+<?php include('v-includes/header.php'); ?>
+	<div class="row row-main-form mrgn-nul">
+		<div class="col-sm-12">
+			<div class="container">
+			
+				<div class="inner-form">
+					<div class="row">
+						<div class="col-xs-4">
+							<div class="line-img">
+							</div>
+						</div>
+						<div class="col-xs-4">
+							<h3 class="head-reg">
+								Update Media
+							</h3>
+						</div>
+						<div class="col-xs-4">
+							<div class="line-img">
+							</div>
+						</div>
+					</div>
+					<h1 class="head-reg-2">
+						Lorem ipsumlorem ip sum lorem ipsumlorem <span class="head-reg">ip</span>
+					</h1>
+					<div class="img-decoration">
+						<img src="images/decoration.png" class="img-responsive" />
+					</div><!-- img-decoration -->
+					
+					<div class="main-form-cont">
+						<div class="form-container">
+							<form class="form-horizontal" role="form" method="post">
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Profile Pic</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Memorial Vid</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 1</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 2</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 3</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 4</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 5</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 6</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 7</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 8</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 9</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								    <label class="col-md-6 control-label label-form">Gallary Pic 10</label>
+								    <div class="col-md-6">
+								      <input type="file" >
+								    </div>
+								 </div>
+								 <div class="form-group">
+								 	<button class="btn btn-primary btn-custom">Submit</button>
+								 </div>
+							</form>
+						</div><!-- form-container -->
+						<div class="row row-deco-btm">
+							<div class="col-xs-4">
+								<img src="images/decoration.png" class="img-responsive" />
+							</div>
+							<div class="col-xs-4">
+								<h3 class="home-head-btm">HOME</h3>
+							</div>
+							<div class="col-xs-4">
+								<img src="images/decoration.png" class="img-responsive" />
+							</div>
+						</div>
+					</div><!-- main-form-cont -->
+					
+				</div><!-- inner form -->
+				
+				
+			</div><!-- container -->
+		</div>
+	</div>
+	
+<?php include('v-includes/footer.php'); ?>
